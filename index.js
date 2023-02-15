ad7c7dda0674e87822ba2044dc0f4671636bddc6
Nee-Shar <99169026+Nee-Shar@users.noreply.github.com>
2023-02-15 17:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -137,6 +137,7 @@ signUpActutal.addEventListener("click",function(){
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
+	     const user = result.user;
             console.log(token);
             // The signed-in user info.
  
